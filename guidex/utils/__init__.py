fe12eff504ffbb071c63100112c42234cb7dde72
Darsh Shrivastava <160412468+shrivdar@users.noreply.github.com>
2025-03-03 20:53:41 -0600

    Update __init__.py

--- guidex/utils/__init__.py
+++ guidex/utils/__init__.py
@@ -1,5 +1,5 @@
 from guidex.utils.logger import setup_logger
-from .utils.exceptions import GenomeFetchError, GrnaDesignError  # Add GrnaDesignError
+from .exceptions import GenomeFetchError, GrnaDesignError  # Add GrnaDesignError
 from guidex.genome_fetcher import GenomeFetcher
 from ..conservation import ConservationAnalyzer
 __all__ = ['GenomeFetcher', 'ConservationAnalyzer']
