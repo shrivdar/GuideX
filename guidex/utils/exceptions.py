e4dae9bf10c02e79c5011f83e7ccf0a191f4a239
Darsh Shrivastava <160412468+shrivdar@users.noreply.github.com>
2025-03-03 21:16:40 -0600

    Update exceptions.py

--- guidex/utils/exceptions.py
+++ guidex/utils/exceptions.py
@@ -1,5 +1,7 @@
 class GenomeFetchError(Exception):
     """Base error for genome fetching failures"""
+    pass
     
 class GrnaDesignError(Exception):
     """Base error for gRNA design failures"""
+    pass
