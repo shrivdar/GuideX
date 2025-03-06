5972cabc5bd34e2edf9d040d23f6a3f0382f2a91
Darsh Shrivastava <160412468+shrivdar@users.noreply.github.com>
2025-03-05 20:16:43 -0600

    Update test_guidex.py

--- test_guidex.py
+++ test_guidex.py
@@ -8,7 +8,7 @@ sys.path.insert(0, str(Path(__file__).parent))
 importlib.invalidate_caches()
 from guidex.genome_fetcher import GenomeFetcher
 from guidex.conservation import ConservationAnalyzer
-from guidex.alignment_engine import AlignmentEngine
+from alignment_engine import AlignmentEngine
 from guidex.core import GuideXGrnaDesigner
 
 LOCAL_GENOMES = [
