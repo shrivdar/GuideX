d3b30e976904e28e6329de6429ccbc8d145defa7
Darsh Shrivastava <160412468+shrivdar@users.noreply.github.com>
2025-03-20 13:46:12 -0500

    Create global_style.py

---
+++ graphs/global_style.py
@@ -0,0 +1,11 @@
+# Set global style (add to all scripts)
+plt.style.use('seaborn')
+sns.set_palette("husl")
+plt.rcParams.update({
+    'font.size': 12,
+    'axes.titlesize': 14,
+    'axes.labelsize': 12,
+    'xtick.labelsize': 10,
+    'ytick.labelsize': 10,
+    'figure.dpi': 300
+})
